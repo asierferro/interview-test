7ab70dc90aca75d46a77f57279071e0f45c0eb42
Asier Ferro <asierferro@gmail.com>
2017-10-26 08:33:20 +0200

    Resolved test

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 export default class Emitter {
-    constructor() { }
+
+    constructor() {
+        this.events = [];
+     }
+
+    subscribe( eventName, fn ) {
+        if( !this.events[eventName] ) {
+        this.events[eventName] = [];
+        }
+        this.events[eventName].push(fn);
+        console.log(this.events)
+        return this.unsubscribe.bind(this, eventName, fn);
+    }
+    
+    unsubscribe(eventName, fn) {
+        const event = this.events[eventName];
+        if( event ) {
+          event.forEach((item, index, object) => {
+             if(item.toString( ) == fn.toString( )){  object.splice(index, 1); }
+           });
+         }
+    }
+ 
+    emit(eventName, data) {
+        const event = this.events[eventName];
+        if( event ) {
+        let args = (arguments.length === 1 ? [arguments[0]] : Array.prototype.slice.call(arguments, 1));
+        let result = [];
+        event.forEach(fn => {
+            let a = fn.apply(null, args);
+            console.log(a);
+           result.push(a);
+        });
+        return result;
+        }
+    }
 }
